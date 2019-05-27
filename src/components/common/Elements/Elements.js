ce84643c1d8beb939f66c3919616a750649651c4
carrot-farm <chohoki@gmail.com>
2019-05-27 10:47:53 +0900

    add lib/tools.js | add components/common/Elements

--- src/components/common/Elements/Elements.js
+++ src/components/common/Elements/Elements.js
@@ -1,24 +1,20 @@
-import React from "react";
-// import { withStyles } from "@material-ui/core/styles";
-// import {
-//   AppBar,
-//   Toolbar,
-//   IconButton,
-//   Drawer,
-//   Button,
-//   List,
-//   ListItem,
-//   ListItemText
-// } from "@material-ui/core";
-// import { Menu, Person, Add } from "@material-ui/icons";
-import styles from "./styles.scss";
+import React, { Component } from "react";
 import classNames from "classnames/bind";
-// import HorizontalContainer from "containers/list/HorizontalContainer";
-// import { Link } from "react-router-dom";
+
+import styles from "./styles.scss";
+import { inputCommSet } from "lib/tools";
 
 const cx = classNames.bind(styles);
 
-export const Container = ({ children, ...args }) => {
+// ===== header
+const Header = ({ children, className, ...args }) => (
+  <header className={cx(`el-header ${className || ""}`)} {...args}>
+    {children}
+  </header>
+);
+
+// ===== container
+const Container = ({ children, ...args }) => {
   return (
     <div className="el-container container" {...args}>
       {children}
@@ -26,14 +22,17 @@ export const Container = ({ children, ...args }) => {
   );
 };
 
-export const Row = ({ children, ...args }) => {
+// ===== row
+const Row = ({ children, ...args }) => {
   return (
     <div className={cx("el-row row")} {...args}>
       {children}
     </div>
   );
 };
-export const Col = ({ children, ...args }) => {
+
+// ===== col
+const Col = ({ children, ...args }) => {
   return (
     <div className={cx("el-col col")} {...args}>
       {children}
@@ -41,8 +40,80 @@ export const Col = ({ children, ...args }) => {
   );
 };
 
+/* ===== inputEvent에 commset를 value로
+  number 와 numberCommSet 을 반환한다.
+  또한, 기본적으로 Enter 와 Backspace 이벤트를 정지한다.
+*/
+class InputCommSet extends Component {
+  state = {
+    number: 0,
+    numberCommSet: 0
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps === this.props || prevState === this.state) {
+      return false;
+    }
+  }
+
+  commSetValue = value => {
+    const { digits } = this.props;
+    const result = inputCommSet(value, digits);
+    // console.log("---> |||| InputCommSet.commSetValue(", result, digits, ")");
+    if (!result) {
+      return;
+    }
+    // console.log("**** pass");
+    this.setState({
+      number: result.number,
+      numberCommSet: result.numberCommSet
+    });
+    return result;
+  };
+
+  handleChange = e => {
+    const { onChange } = this.props;
+    const { value } = e.target;
+    const result = this.commSetValue(value);
+    // console.log("handleChange.result: ", returnData);
+    if (onChange) {
+      onChange({
+        ...e,
+        sendData: result
+      });
+    }
+  };
+
+  // 키동작 관련
+  handleKeyDown = e => {
+    const { onKeyDown, useEnter } = this.props;
+    const result = this.commSetValue(e.target.value);
+    if (!useEnter && e.key === "Enter") {
+      e.preventDefault();
+    } else if (onKeyDown) {
+      onKeyDown({
+        ...e,
+        sendData: result
+      });
+    }
+  };
+
+  render() {
+    const { fixed, onChange, onKeyDown, digits, ...args } = this.props;
+    return (
+      <input
+        onChange={this.handleChange}
+        onKeyDown={this.handleKeyDown}
+        {...args}
+      />
+    );
+  }
+}
+
 export default {
+  Header,
   Container,
   Row,
-  Col
+  Col,
+  InputCommSet
 };
